d533416cc1896325daddbb216bd9f9d1b5e9c22a
kakorotto <paulaatiya@gmail.com>
2025-10-26 07:19:52 +0300

    chore(debug): render DummyRoot in index to verify deploy

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import DummyRoot from "./DummyRoot";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<DummyRoot />, document.getElementById("root"));
